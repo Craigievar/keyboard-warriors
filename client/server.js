af0358b4a8af2d2858a2acb45e254c2b8350c54c
craigievar <craig@statsig.com>
2023-11-09 22:00:17 -0800

    ree

--- client/server.js
+++ client/server.js
@@ -5,8 +5,9 @@ var path = require("path");
 var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(path.join(__dirname, "../client/public")));
+
+app.use(express.static(path.join(__dirname, "../public")));
 console.log(path.join(__dirname, "../public"));
 app.listen(PORT, function () {
-    console.log("Server running on port ".concat(PORT));
+  console.log("Server running on port ".concat(PORT));
 });
