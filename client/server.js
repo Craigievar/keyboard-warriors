6e1eb191cc3aeb7790b3e512429ab02bd1f977a1
craigievar <craig@statsig.com>
2023-11-09 22:03:48 -0800

    ree

--- client/server.js
+++ client/server.js
@@ -3,11 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 var path = require("path");
 var express = require("express");
+var fs = require("fs");
 var PORT = process.env.PORT || 3000;
 var app = express();
-
-app.use(express.static(path.join(__dirname, "../public")));
-console.log(path.join(__dirname, "../public"));
+app.get("/", function (req, res) {
+    // Specify the path to your index.html file
+    var indexPath = path.join(__dirname, "public/index.html");
+    // Check if the file exists
+    fs.access(indexPath, fs.constants.F_OK, function (err) {
+        if (err) {
+            console.error("File does not exist:", err);
+            return res.status(404).send("404 Not Found");
+        }
+        // If the file exists, read it and send it in the response
+        fs.readFile(indexPath, function (err, data) {
+            if (err) {
+                console.error("Error reading file:", err);
+                return res.status(500).send("Internal Server Error");
+            }
+            res.contentType("text/html").send(data);
+        });
+    });
+});
 app.listen(PORT, function () {
-  console.log("Server running on port ".concat(PORT));
+    console.log("Server running on port ".concat(PORT));
 });
--- client/server.ts
+++ client/server.ts
@@ -3,13 +3,33 @@ require("dotenv").config();
 import path = require("path");
 import express = require("express");
 
+const fs = require("fs");
+
 const PORT: number | string = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../client/public")));
-console.log(path.join(__dirname, "../public"));
+app.get("/", (req, res) => {
+  // Specify the path to your index.html file
+  const indexPath = path.join(__dirname, "public/index.html");
 
+  // Check if the file exists
+  fs.access(indexPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error("File does not exist:", err);
+      return res.status(404).send("404 Not Found");
+    }
+
+    // If the file exists, read it and send it in the response
+    fs.readFile(indexPath, (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        return res.status(500).send("Internal Server Error");
+      }
+      res.contentType("text/html").send(data);
+    });
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
